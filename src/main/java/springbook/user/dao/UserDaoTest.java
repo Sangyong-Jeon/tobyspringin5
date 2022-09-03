540353553214d01d3e4db63566e29740ad584a85
JeonSangYong <tkddyd420@naver.com>
2022-09-03 23:20:57 +0900

    1.4.1 오브젝트 팩토리

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,14 +1,23 @@
 package springbook.user.dao;
 
+import springbook.user.domain.User;
+
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        // UserDao가 사용할 ConnectionMaker 구현 클래스를 결정하고 오브젝트를 만든다
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-        // 1. UserDao 생성
-        // 2. 사용할 ConnectionMaker 타입의 오브젝트 제공
-        // => 결국 두 오브젝트 사이의 의존관계 설정 효과
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
+        
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
     }
 }
